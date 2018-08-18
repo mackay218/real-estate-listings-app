a81071185157d89ec2bd38928852ef40dd6a0b0c
Daniel MacKay <mackay2588@gmail.com>
2018-08-18 12:56:27 -0500

    sales controller/ get route/ view work

--- server/public/scripts/controllers/sale.controller.js
+++ server/public/scripts/controllers/sale.controller.js
@@ -1,16 +1,29 @@
-houseApp.controller('SalesContoller', ['$http', function ($http) {
-    const rc = this;
+houseApp.controller('SalesController', ['$http', function ($http) {
+    const sc = this;
+    
+    //array to hold response
+    sc.salesArr = [];
 
     //GET route
     function getSales() {
         console.log('in getSales');
         $http({
             method: 'GET',
-            url: '/sales'
+            url: '/listings'
         }).then(function (response) {
-            console.log(response);
+            console.log(response.data);
+
+            for (let house of response.data) {
+                if (house.type == 'sale') {
+                    sc.salesArr.push(house);
+                }
+            }
+            console.log(sc.salesArr);
+
         }).catch(function (error) {
             console.log('error in getSales:', error);
         });
     };
+
+    getSales();
 }]);
